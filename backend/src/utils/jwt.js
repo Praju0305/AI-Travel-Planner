e75af6df86b0f98b02ce17a5fd7f20351dc41bfa
Praju0305 <prajwalbm0305@gmail.com>
2026-06-27 23:49:56 +0530

    feat: add JWT utility functions

---
+++ backend/src/utils/jwt.js
@@ -0,0 +1,26 @@
+import jwt from "jsonwebtoken";
+
+export const generateToken = (userId) => {
+  return jwt.sign(
+    { id: userId },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: process.env.JWT_EXPIRES_IN || "7d",
+    }
+  );
+};
+
+export const sendTokenResponse = (user, statusCode, res) => {
+  const token = generateToken(user._id);
+
+  res.status(statusCode).json({
+    success: true,
+    token,
+    user: {
+      id: user._id,
+      name: user.name,
+      email: user.email,
+      preferences: user.preferences,
+    },
+  });
+};
